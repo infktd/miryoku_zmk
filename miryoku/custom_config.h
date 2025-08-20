d2f484f96c341a70219a2328292ab30d89485beb
= <=>
2025-08-20 00:24:15 -0500

    Remove MIRYOKU_LAYER_EXTRA definition from custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -62,10 +62,3 @@ U_NP,               &kp F12,            &kp F7,               &kp F8,           
 U_NP,               &kp F11,            &kp F4,               &kp F5,               &kp F6,               &kp SLCK,            U_NA,               &kp LSHFT,          &kp LCTRL,          &kp LALT,           &kp LGUI,           U_NP,               \
 U_NP,               &kp F10,            &kp F1,               &kp F2,               &kp F3,               &kp PAUSE_BREAK,     U_NA,               &u_to_U_FUN,        &u_to_U_MEDIA,      &kp RALT,           U_NA,               U_NP,               \
 U_NP,               U_NP,               U_NP,                &kp K_APP,            &kp SPACE,            &kp TAB,             U_NA,               U_NA,               U_NA,               U_NP,               U_NP,               U_NP
-
-
-#define MIRYOKU_LAYER_EXTRA \
-&kp GRAVE,           &kp Q,              &kp W,               &kp E,               &kp R,               &kp T,               &kp Y,               &kp U,               &kp I,               &kp O,               &kp P,                    &kp BSLH,   \
-&kp TAB,             U_MT(LGUI, A),      U_MT(LALT, S),       U_MT(LCTRL, D),      U_MT(LSHFT, F),      &kp G,               &kp H,               U_MT(LSHFT, J),      U_MT(LCTRL, K),      U_MT(LALT, L),       U_MT(LGUI, SEMI),         &kp SQT,    \
-U_NP,                U_LT(U_BUTTON, Z),  U_MT(RALT, X),       &kp C,               &kp V,               &kp B,               &kp N,               &kp M,               &kp COMMA,           U_MT(RALT, DOT),    U_LT(U_BUTTON, SLASH),    U_NP,       
-U_NP,                U_NP,               U_NP,                U_LT(U_MEDIA, ESC),  U_LT(U_NAV, SPACE),  U_LT(U_MOUSE, TAB),  U_LT(U_SYM, RET),    U_LT(U_NUM, BSPC),   U_LT(U_FUN, DEL),   U_NP,               U_NP,                     U_NP
